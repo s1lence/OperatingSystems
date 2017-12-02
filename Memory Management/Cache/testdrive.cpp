ba43f3d651e705acb0f6b4b59b0642f16af7156f
Ruslan Neshta <Ruslan.Neshta@gmail.com>
2017-12-02 14:03:14 +0200

    entry created

---
+++ Memory Management/Cache/testdrive.cpp
@@ -0,0 +1,30 @@
+/*
+ * `file`          testdrive.cpp
+ * `written`       December 02 2017 13:38:56
+ * `last modified` December 02 2017 13:38:56
+ * 
+ * Author:         R. Neshta
+ * Contact:        Ruslan.Neshta@gmail.com
+ * 
+ * `synopsis`      program entry
+ * 
+ * Description:    testing cache model
+ *                 
+ * 
+ * `note`          none
+ */
+ 
+#include "Cache.h"
+
+int main(){
+
+  win32::Cache<> cache;
+  win32::dword arr[112];
+  for (win32::dword i=0;i<112;++i)
+    arr[i] = i;
+
+  cache.initMemory(arr, 112);
+  cache.stepByStepDebugTest();
+
+  return 0;
+}
